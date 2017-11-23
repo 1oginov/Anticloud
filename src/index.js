5d7b5ca4bbaf05f9d0ec30cc14c39b5fcb0e81ee
Danila Loginov <loginovdanila@gmail.com>
2017-11-24 02:50:17 +0300

    refactor: removed unused assets created by Create React App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root')); // eslint-disable-line react/jsx-filename-extension
-registerServiceWorker();
+// TODO: Check if it is actual for the app
+// registerServiceWorker();
