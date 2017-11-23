42fe38ae8939d71536b6e157acd75eecd70f4c15
Danila Loginov <loginovdanila@gmail.com>
2017-11-24 01:57:15 +0300

    feat: Entry class extended with Stats cache, checking if directory and opening if file

--- src/classes/Entry.js
+++ src/classes/Entry.js
@@ -2,6 +2,7 @@ import { remote } from '../services/electron';
 
 const fs = remote.require('fs');
 const path = remote.require('path');
+const { shell } = remote;
 
 /**
  * Entry class.
@@ -14,6 +15,7 @@ class Entry {
   constructor(entryPath) {
     this.path = entryPath;
     this.key = this.path;
+    this.statsCache = undefined;
   }
 
   /**
@@ -21,6 +23,10 @@ class Entry {
    * @returns {Promise.<(Stats|Error)>}
    */
   getStats() {
+    if (this.statsCache !== undefined) {
+      return Promise.resolve(this.statsCache);
+    }
+
     return new Promise((resolve, reject) => {
       fs.stat(this.path, (error, stats) => {
         if (error) {
@@ -28,9 +34,19 @@ class Entry {
           return;
         }
 
+        this.statsCache = stats;
         resolve(stats);
       });
     });
+  }
+
+  /**
+   * Is directory?
+   * @returns {Promise.<boolean|Error>}
+   */
+  isDirectory() {
+    return this.getStats()
+      .then(stats => stats.isDirectory());
   }
 
   /**
@@ -38,16 +54,38 @@ class Entry {
    * @returns {Promise.<(Array.<Entry>|Error)>}
    */
   getContents() {
-    return new Promise((resolve, reject) => {
-      fs.readdir(this.path, (error, entriesNames) => {
-        if (error) {
-          reject(error);
-          return;
+    return this.isDirectory()
+      .then((isDirectory) => {
+        if (!isDirectory) {
+          return Promise.reject(new Error(`${this.path} is not a directory`));
         }
 
-        resolve(entriesNames.map(entryName => new Entry(path.join(this.path, entryName))));
+        return new Promise((resolve, reject) => {
+          fs.readdir(this.path, (error, entriesNames) => {
+            if (error) {
+              reject(error);
+              return;
+            }
+
+            resolve(entriesNames.map(entryName => new Entry(path.join(this.path, entryName))));
+          });
+        });
       });
-    });
+  }
+
+  /**
+   * Open.
+   * @returns {Promise.<(boolean|Error)>}
+   */
+  open() {
+    return this.isDirectory()
+      .then((isDirectory) => {
+        if (isDirectory) {
+          return Promise.reject(new Error(`${this.path} is a directory`));
+        }
+
+        return shell.openItem(this.path);
+      });
   }
 }
 
